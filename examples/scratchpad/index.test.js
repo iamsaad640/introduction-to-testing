58f6e24772d2430f0313c8682cbea430a8414f03
Saad <iamsaad640@gmail.com>
2024-12-07 18:53:11 +0500

    mock

--- examples/scratchpad/index.test.js
+++ examples/scratchpad/index.test.js
@@ -1,5 +1,12 @@
-import { test, expect } from 'vitest';
+import { test, expect, vi } from 'vitest';
+//spies are a tool used to observe and track the behavior of functions during test execution. Spies help verify whether a function was called, how many times it was called, and what arguments were passed to it
+
+const spyLog = vi.spyOn(console, 'log');
 
 test('a super simple test', () => {
-  expect(true).toBe(true);
+  console.log('Hello world!');
+  expect(spyLog).toHaveBeenCalled();
+  expect(spyLog).toHaveBeenCalledWith('Hello world!');
+  expect(spyLog).toHaveBeenCalledOnce();
+  expect(spyLog).toHaveBeenCalledTimes(1);
 });
